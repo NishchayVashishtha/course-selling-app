f66b765d157d9cf6c146f55856604417c69747af
NishchayVashishtha <nishchayvashishtha04@gmail.com>
2026-02-19 14:05:37 +0530

    updating admin and user page

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,85 @@
 const { Router } = require("express");
 const adminRouter = Router();
 const { adminModel } = require("../db");
+const { z } = require("zod");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const JWT_ADMIN_SECRET = "hesoyam";
 
-adminRouter.post("/signup", function(req, res) {
-    message.json({
-        message : "admin signup Endpoint!"
+adminRouter.post("/signup", async function(req, res) {
+    
+    const requireBody = z.object({
+        email : z.string().min(3).max(100).email(),
+        password : z.string().min(3).max(20),
+        firstName : z.string().min(3).max(20),
+        lastName : z.string().min(3).max(20) 
     })
+
+    const parsedDataWithSuccess = requireBody.safeParse(req.body); 
+
+    if(!parsedDataWithSuccess.success) {
+        res.json({
+            message : "Incorrect Format"
+        })
+        return;
+    }
+
+    //hash the password
+    const {email, password, firstName, lastName } = req.body;
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 5);
+
+        await adminModel.create({
+            email : email,
+            password : hashedPassword,
+            firstName : firstName,
+            lastName : lastName
+        })
+        res.json({
+        message : "signup succeeded!"
+        })
+        throw new Error("User Already Exists");
+
+    } catch(e) {
+        console.error(e);
+        res.status(500).json({
+            message : "Error while signing-up"
+        })
+    }
 })
 
-adminRouter.post("/login", function(req, res) {
-    message.json({
-        message : "admin login Endpoint!"
+adminRouter.post("/login",async function(req, res) {
+    const { email, password } = req.body;
+
+    const admin = await adminModel.findOne({
+        email : email
     })
+
+    if(!admin) {
+        return res.status(403).json({
+            message : "User does not exist in our database"
+        });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, admin.password);
+
+
+    if(passwordMatch) {
+        const token = jwt.sign({
+            id : admin._id.toString()
+        }, JWT_ADMIN_SECRET);
+
+        //Do Cookie Logic
+
+        res.json({
+            token : token
+        })
+    } else {
+        res.status(403).json({
+            message : "Incorrect credentials"
+        })
+    }
 })
 
 adminRouter.post("/", function(req, res) {
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const { Router } = require('express');
 const { userModel } = require("../db");
 const { z } = require("zod");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const JWT_USER_SECRET = "aezakmi";
 
 const userRouter = Router();
 
@@ -49,10 +51,37 @@ userRouter.post("/signup", async function(req, res) {
     }
 }) 
 
-userRouter.post("/login", function(req, res) {
-    res.json({
-        message : "login endpoint"
+userRouter.post("/login", async function(req, res) {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({
+        email : email
     })
+
+    if(!user) {
+        return res.status(403).json({
+            message : "User does not exist in our database"
+        });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+
+    if(passwordMatch) {
+        const token = jwt.sign({
+            id : user._id.toString()
+        }, JWT_USER_SECRET);
+
+        //Do Cookie Logic
+
+        res.json({
+            token : token
+        })
+    } else {
+        res.status(403).json({
+            message : "Incorrect credentials"
+        })
+    }
 })
 
 userRouter.get("/purchases", function(req, res) {
